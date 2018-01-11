cb1a6c638504f2edff96fd63059b9f448d17e759
rafaeel16 <rafaeel.16@gmail.com>
2018-01-10 22:54:54 -0200

    finalization of crud operations

--- webservice-restfull/src/http/routes.js
+++ webservice-restfull/src/http/routes.js
@@ -12,19 +12,38 @@ const routes = (server) => {
     }
   })
 
-  server.post('categoria', (req, res, next) => {
+  server.post('categoria', async (req, res, next) => {
     const { name } = req.params
-    res.send(name)
-    next()
+    try {
+      res.send(await db.categories().save(name))
+      next()
+    } catch (error) {
+      res.send(error)
+      next()
+    }
   })
-  //   server.put('categoria', (req, res, next) => {
-  //     res.send()
-  //     next()
-  //   })
-  //   server.delete('categoria', (req, res, next) => {
-  //     res.send()
-  //     next()
-  //   })
+  server.put('categoria', async (req, res, next) => {
+    const { id, name } = req.params
+    try {
+      res.send(await db.categories().update(id, name))
+      next()
+    } catch (error) {
+      res.send(error)
+      next()
+    }
+  })
+
+  server.del('categoria', async (req, res, next) => {
+    const { id } = req.params
+    try {
+      res.send(await db.categories().del(id))
+      next()
+    } catch (error) {
+      res.send(error)
+      next()
+    }
+  })
+
   server.get('/', (req, res, next) => {
     res.send('Enjoy the silence?')
     next()
