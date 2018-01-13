f7e2c7cb845d32734de86596bb0764bc6fe5bfe4
rafaeel16 <rafaeel.16@gmail.com>
2018-01-12 22:23:21 -0200

    Refactoring connection database, add .env

--- webservice-restfull/src/services/mysql/index.js
+++ webservice-restfull/src/services/mysql/index.js
@@ -2,10 +2,10 @@
 const mysqlServer = require('mysql')
 
 const connection = mysqlServer.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'restful_ws'
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 })
 
 const errorHandler = (error, msg, rejectFunction) => {
