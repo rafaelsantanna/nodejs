76429695f7de7fb1bdfb596280de0716ce69af71
rafaeel16 <rafaeel.16@gmail.com>
2018-01-14 14:17:08 -0200

    crud tests finalized

--- webservice-restfull/src/services/mysql/tests/categories.test.js
+++ webservice-restfull/src/services/mysql/tests/categories.test.js
@@ -1,25 +1,31 @@
 
 const test = require('ava')
+const { connection, errorHandler } = require('./setup')
+const categories = require('../categories')({ connection, errorHandler })
+const create = () => categories.save('category-test')
 
-require('dotenv').config()
+test.beforeEach(t => connection.query('TRUNCATE TABLE categories'))
+test.after.always(t => connection.query('TRUNCATE TABLE categories'))
 
-const mysqlServer = require('mysql')
-
-const connection = mysqlServer.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
+test('Lista de categoria', async t => {
+  await create()
+  const list = await categories.all()
+  t.is(list.categories.length, 1)
 })
 
-const errorHandler = (error, msg, rejectFunction) => {
-  console.error(error)
-  rejectFunction({ error: msg })
-}
-
-const categories = require('../categories')({ connection, errorHandler })
-
 test('Criação de categoria', async t => {
-  const result = await categories.save('category-test')
+  const result = await create()
   t.is(result.category.name, 'category-test')
+})
+
+test('Atualização de categoria', async t => {
+  await create()
+  const updated = await categories.update(1, 'category-test-updated')
+  t.is(updated.category.name, 'category-test-updated')
+})
+
+test('Remoção de categoria', async t => {
+  await create()
+  const removed = await categories.del(1)
+  t.is(removed.affectedRows, 1)
 })
